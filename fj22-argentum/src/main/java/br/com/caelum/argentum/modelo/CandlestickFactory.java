5dc59b6cfb54b3fe8800f0bf756d1136cb5e3f84
Edi Carlos note <edi_csilva@hotmail.com>
2017-02-03 18:02:10 -0200

    testando modelo sem framework 3.2 até item 3.

--- fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
+++ fj22-argentum/src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -8,8 +8,8 @@ public class CandlestickFactory {
 	public Candlestick constroiCandleParaData(Calendar data,
 						List<Negociacao> negociacoes ) {
 		
-		double maximo = negociacoes.get(0).getPreco();
-		double minimo = negociacoes.get(0).getPreco();
+		double maximo = 0;
+		double minimo = Double.MAX_VALUE;
 		
 		double volume = 0;
 		
@@ -23,8 +23,8 @@ public class CandlestickFactory {
 			}
 		}
 		
-		double abertura = negociacoes.get(0).getPreco();
-		double fechamento = negociacoes.get(negociacoes.size() - 1).getPreco();
+		double abertura = negociacoes.isEmpty() ? 0 : negociacoes.get(0).getPreco();
+		double fechamento = negociacoes.isEmpty() ? 0 : negociacoes.get(negociacoes.size() - 1).getPreco();
 		
 		
 		return new Candlestick (abertura, fechamento, minimo, maximo, volume, data);
